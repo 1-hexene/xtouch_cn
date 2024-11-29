68d5154e897863ad69fd0c5828c1547a6950d00b
王淑宁 <qizixi2@126.com>
2024-11-29 16:24:48 +1100

    Revert "暂存"
    This reverts commit b7fcbf7d5f9f46594fd596918271a45c9ff9e4fa.

--- src/xtouch/sdcard.h
+++ src/xtouch/sdcard.h
@@ -59,25 +59,23 @@ bool xtouch_sdcard_setup()
     return true;
 }
 #else
-void plot_fs_msg()
-{
-    // 获取SPIFFS的信息
-    uint32_t totalBytes = SPIFFS.totalBytes();
-    uint32_t usedBytes = SPIFFS.usedBytes();
-    // 计算剩余空间
-    uint32_t freeBytes = totalBytes - usedBytes;
+void plot_fs_msg(){
+      // 获取SPIFFS的信息
+  uint32_t totalBytes = SPIFFS.totalBytes();
+  uint32_t usedBytes = SPIFFS.usedBytes();
+  // 计算剩余空间
+  uint32_t freeBytes = totalBytes - usedBytes;
 
-    // 打印信息
-    Serial.print("Total bytes: ");
-    Serial.println(totalBytes);
-    Serial.print("Used bytes: ");
-    Serial.println(usedBytes);
-    Serial.print("Free bytes: ");
-    Serial.println(freeBytes);
+  // 打印信息
+  Serial.print("Total bytes: ");
+  Serial.println(totalBytes);
+  Serial.print("Used bytes: ");
+  Serial.println(usedBytes);
+  Serial.print("Free bytes: ");
+  Serial.println(freeBytes);
 }
 
-bool xtouch_sdcard_setup()
-{
+bool xtouch_sdcard_setup(){
     lv_label_set_text(introScreenCaption, LV_SYMBOL_SD_CARD "尝试初始化内部存储器");
     lv_obj_set_style_text_color(introScreenCaption, lv_color_hex(0xFF0000), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_timer_handler();
@@ -97,31 +95,12 @@ bool xtouch_sdcard_setup()
     // file.close();
     plot_fs_msg();
 
-    //xtouch_filesystem_mkdir(SPIFFS, xtouch_paths_root);
-    /* if (!xtouch_filesystem_exist(SPIFFS, xtouch_paths_config)){
+    // xtouch_filesystem_mkdir(SPIFFS, xtouch_paths_root);
+    if (!xtouch_filesystem_exist(SPIFFS, xtouch_paths_config)){
         lv_label_set_text(introScreenCaption, LV_SYMBOL_USB "请连接电脑软件进行初始化设置");
         lv_obj_set_style_text_color(introScreenCaption, lv_color_hex(0xFF0000), LV_PART_MAIN | LV_STATE_DEFAULT);
         lv_timer_handler();
         return false;
-    } */
-    if (!xtouch_filesystem_exist(SPIFFS, xtouch_paths_config))
-    {
-        lv_label_set_text(introScreenCaption, LV_SYMBOL_USB "正在写入配置");
-        lv_obj_set_style_text_color(introScreenCaption, lv_color_hex(0xFF0000), LV_PART_MAIN | LV_STATE_DEFAULT);
-        lv_timer_handler();
-
-        xtouch_filesystem_mkdir(SPIFFS, xtouch_paths_root);
-        
-        File configFile = SPIFFS.open(xtouch_paths_config, FILE_WRITE);
-        StaticJsonDocument<1024> config;
-        config["ssid"] = "88888888";
-        config["pwd"] = "88888888";
-        config["cloud-email"] = "88888888";
-        config["cloud-password"] = "88888888";
-        config["user-id"] = "88888888";
-        config["cloud-region"] = "China";
-        serializeJson(config, configFile);
-        configFile.close();
     }
     return true;
 }
